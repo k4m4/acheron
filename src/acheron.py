3fa87cb75da29d114854dd475864e047b3b39a17
k4m4 <nikolaskam@gmail.com>
2023-05-14 18:04:35 -0400

    Read torrent file path from args

--- src/acheron.py
+++ src/acheron.py
@@ -1,30 +1,40 @@
 from torrent import Torrent
 from secrets import token_bytes
-from pprint import pprint
 import logging
 import warnings
+import argparse
 
 LISTEN_PORT = 6881
 CLIENT_ID = b'-AH0001-'
+VERSION = '0.1.0'
+CLIENT_NAME = 'Acheron'
 
 # TODO: listen
 
 class Client:
-  def __init__(self):
+  def __init__(self, torrent_file):
     logging.basicConfig(level=logging.DEBUG)
 
-    logging.info('Acheron v0.0.1 - A torrent client')
+    logging.info(f'{CLIENT_NAME} {VERSION} - A torrent client')
 
     self.peer_id = CLIENT_ID + token_bytes(20 - len(CLIENT_ID))
     self.listen_port = LISTEN_PORT
 
-    with open('fixtures/ubuntu-23.04-desktop-amd64.iso.torrent', 'rb') as f:
+    with open(torrent_file, 'rb') as f:
       metadata = f.read()
       torrent = Torrent(self, metadata)
 
 def main():
+  parser = argparse.ArgumentParser(
+    prog='acheron',
+    description='A BitTorrent client',
+    epilog=f'{CLIENT_NAME} {VERSION} - A torrent client'
+  )
+  parser.add_argument('-v', '--version', action='version', version='%(prog)s {VERSION}')
+  parser.add_argument('torrent_file', help='path to .torrent file', type=str)
+  args = parser.parse_args()
   warnings.filterwarnings("error", category=RuntimeWarning)
-  client = Client()
+  client = Client(torrent_file=args.torrent_file)
 
 if __name__ == '__main__':
   main()
